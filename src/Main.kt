72789c0949c212c48ef8bdcfd2a76f79cebf1af5
Ivan Martinez <iavanmartinez12@gmail.com>
2025-09-25 18:26:46 +0200

    he creado una clase

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    val nombre: String = "Ana"
+    val edad: Int = 25
+    val altura: Double = 1.68
+    val usaGafas: Boolean = true
+
+    println("Mi nombre es $nombre, tengo $edad años, mido $altura metros y ${if (usaGafas) 
+        "uso gafas" else "no uso gafas"}.")
 }
+
